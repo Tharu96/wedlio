cbcfd9216ae278767060783ba20d318835522b04
khguruge <40533227+khguruge@users.noreply.github.com>
2020-01-09 22:47:54 +0530

    budgeter

--- application/views/customer/budgeter.php
+++ application/views/customer/budgeter.php
@@ -1,7 +1,7 @@
 <html>
 	<head>
 		<title>Sum Html Textbox Values using jQuery/JavaScript</title>
-		<style>
+		<!-- <style>
 			body {
 				font-family: sans-serif;
 			}
@@ -15,41 +15,70 @@
 				font-weight: bold;
 				text-align: right;
 			}
-		</style>
+		</style> -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
 	</head>
+
 	<body>
-<table width="300px" border="1" style="border-collapse:collapse;background-color:#E8DCFF">
-	<tr>
-		<td width="40px">1</td>
-		<td>Butter</td>
-		<td><input class="txt" type="text" name="txt"/></td>
-	</tr>
-	<tr>
-		<td>2</td>
-		<td>Cheese</td>
-		<td><input class="txt" type="text" name="txt"/></td>
-	</tr>
-	<tr>
-		<td>3</td>
-		<td>Eggs</td>
-		<td><input class="txt" type="text" name="txt"/></td>
-	</tr>
-	<tr>
-		<td>4</td>
-		<td>Milk</td>
-		<td><input class="txt" type="text" name="txt"/></td>
-	</tr>
-	<tr>
-		<td>5</td>
-		<td>Bread</td>
-		<td><input class="txt" type="text" name="txt"/></td>
-	</tr>
-	<tr>
-		<td>6</td>
-		<td>Soap</td>
-		<td><input class="txt" type="text" name="txt"/></td>
-	</tr>
+
+    <div class="container">
+    <form>
+        <div class="form-group row">
+            <label for="inputEmail3" class="col-sm-2 col-form-label">Reception Venues</label>
+            <div class="col-sm-10">
+            <input type="email" class="txt" id="inputEmail3" placeholder="Email">
+            </div>
+        </div>
+        <div class="form-group row">
+            <label for="inputEmail3" class="col-sm-2 col-form-label">Videography</label>
+            <div class="col-sm-10">
+            <input type="email" class="txt" id="inputEmail3" placeholder="Email">
+            </div>
+        </div>
+        <div class="form-group row">
+            <label for="inputEmail3" class="col-sm-2 col-form-label">Photography</label>
+            <div class="col-sm-10">
+            <input type="email" class="txt" id="inputEmail3" placeholder="Email">
+            </div>
+        </div>
+        <div class="form-group row">
+            <label for="inputEmail3" class="col-sm-2 col-form-label">Wedding Bands</label>
+            <div class="col-sm-10">
+            <input type="email" class="txt" id="inputEmail3" placeholder="Email">
+            </div>
+        </div>
+        <div class="form-group row">
+            <label for="inputEmail3" class="col-sm-2 col-form-label">Bridal Salon</label>
+            <div class="col-sm-10">
+            <input type="email" class="txt" id="inputEmail3" placeholder="Email">
+            </div>
+        </div>
+        <div class="form-group row">
+            <label for="inputEmail3" class="col-sm-2 col-form-label">Djs</label>
+            <div class="col-sm-10">
+            <input type="email" class="txt" id="inputEmail3" placeholder="Email">
+            </div>
+        </div>
+        <div class="form-group row">
+            <label for="inputEmail3" class="col-sm-2 col-form-label">Florist</label>
+            <div class="col-sm-10">
+            <input type="email" class="txt" id="inputEmail3" placeholder="Email">
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <label for="inputEmail3" class="col-sm-2 col-form-label">Sum : </label>
+            <div class="col-sm-10">
+            <input type="email" class="txt" id="sum" placeholder="Email">
+            </div>
+        </div>
+        
+        </form>
+    </div>
+
+
 	<tr id="summation">
 		<td>&nbsp;</td>
 		<td align="right">Sum :</td>
