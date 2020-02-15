93407f36c7b9005bc0d8c1f29666b8f6668d648d
khguruge <40533227+khguruge@users.noreply.github.com>
2020-02-15 19:26:11 +0530

    update_wedding_details

--- application/views/customer/updateCustomer.php
+++ application/views/customer/updateCustomer.php
@@ -1,66 +1,65 @@
 
 <h3>Your Wedding Details</h3>
 <br><br>
-<<<<<<< HEAD
+<p><?php echo  $this->session->userdata('id'); ?></p>
+<?php extract($customerData); ?>
+<?php echo $weddingDistrict; ?>
 
-=======
-<p><?php echo  $this->session->userdata('email'); ?></p>
->>>>>>> 0438689a3f0eb88e7634ae0e4a4028e409974674
-<form action="" >
 
+<form method="POST" action="<?php echo base_url(); ?>index.php/updateCustomerController/update/<?php echo  $this->session->userdata('id'); ?>">
 <div class="form-row">
 	<div class="form-group col-md-6">
-		<label for="inputfirstname">Engagement Date</label>
-		<input class="form-control" type="date" value="2014-04-07" id="example-date-input">
+		<label >Engagement Date</label>
+		<input class="form-control" type="date" value="<?php echo $engagementDate; ?>" name="engagementDate">
 	</div>
 	<div class="form-group col-md-6">
-    <label for="inputfirstname">Wedding Date</label>
-		<input class="form-control" type="date" value="2014-04-07" id="example-date-input">
+    <label >Wedding Date</label>
+		<input class="form-control" type="date" value="<?php echo $weddingDate; ?>" name="weddingDate">
 	</div>
 </div>
-
-
 <div class="form-row">
 	<div class="form-group col-md-6">
-		<label for="inputfirstname">Wedding City</label>
-		<input class="form-control" type="text" value="" id="example-text-input" placeholder="Wedding City">
+		<label >Wedding City</label>
+		<input class="form-control" type="text" value="<?php echo $weddingCity; ?>"  name="weddingCity">
 	</div>
 	<div class="form-group col-md-6">
-		<label for="inputfirstname">wedding District</label>
-		<select class="form-control" id="sel1">
-            <option value="1">Ampara</option>
-		    <option value="2">Anuradhapura</option>
-		    <option value="3">Badulla</option>
-		    <option value="4">Batticaloa</option>
-		    <option value="5">Colombo</option>
-		    <option value="6">Galle</option>
-		    <option value="7">Gampaha</option>
-		    <option value="8">Hambantota</option>
-		    <option value="9">Jaffna</option>
-			<option value="10">Kalutara</option>
-			<option value="11">Kandy</option>
-			<option value="12">Kegalle</option>
-			<option value="13">Kilinochchi</option>
-			<option value="14">Kurunegala</option>
-			<option value="15">Mannar</option>
-			<option value="16">Mathale</option>
-			<option value="17">Matara</option>
-			<option value="18">Monaragala</option>
-			<option value="19">Mullaitivu</option>
-			<option value="20">Nuwara Eliya</option>
-			<option value="21">Polonnaruwa</option>
-			<option value="22">Puttalam</option>
-			<option value="23">Ratnapura</option>
-			<option value="24">Trincomalee</option>
-			<option value="25">Vavuniya</option>
+		<label >wedding District</label>
+		<select class="form-control" name="weddingDistrict" >
+			<option value=<?php echo $weddingDistrict; ?> selected ><?php echo $weddingDistrict; ?></option>
+            <option value="Ampara">Ampara</option>
+		    <option value="Anuradhapura">Anuradhapura</option>
+		    <option value="Badulla">Badulla</option>
+		    <option value="Batticaloa">Batticaloa</option>
+		    <option value="Colombo">Colombo</option>
+		    <option value="Galle">Galle</option>
+		    <option value="Gampaha">Gampaha</option>
+		    <option value="Hambantota">Hambantota</option>
+		    <option value="Jaffna">Jaffna</option>
+			<option value="Kalutara">Kalutara</option>
+			<option value="Kandy">Kandy</option>
+			<option value="Kegalle">Kegalle</option>
+			<option value="Kilinochchi">Kilinochchi</option>
+			<option value="Kurunegala">Kurunegala</option>
+			<option value="Mannar">Mannar</option>
+			<option value="Mathale">Mathale</option>
+			<option value="Matara">Matara</option>
+			<option value="Monaragala">Monaragala</option>
+			<option value="Mullaitivu">Mullaitivu</option>
+			<option value="Nuwara Eliya">Nuwara Eliya</option>
+			<option value="Polonnaruwa">Polonnaruwa</option>
+			<option value="Puttalam">Puttalam</option>
+			<option value="Ratnapura">Ratnapura</option>
+			<option value="Trincomalee">Trincomalee</option>
+			<option value="Vavuniya">Vavuniya</option>
       </select>
 	</div>
 </div>
 
 <div class="form-row">
 	<div class="form-group col-md-6">
-		<label for="inputfirstname">Number Of wedding Guests</label>
-		<select class="form-control" id="sel1">
+		<label>Number Of wedding Guests</label>
+		<select class="form-control" name="numberOfWeddingGuests" >
+		<option value=<?php echo $numberOfWeddingGuests; ?> selected ><?php echo $numberOfWeddingGuests; ?></option>
         <option>0-50</option>
         <option>51-100</option>
         <option>101-150</option>
@@ -73,34 +72,20 @@
 
 <div class="form-row">
 	<div class="form-group col-md-6">
-    <label for="inputfirstname">Wedding Budget</label>
-    <select class="form-control" id="sel1">
-        <option>Rs: 0 - Rs: 500,000</option>
-        <option>Rs: 500,000 - Rs: 750,000</option>
-        <option>Rs: 750,000 - Rs: 1,000,000</option>
-        <option>Rs: 1,000,000 - Rs: 1,500,000</option>
-        <option>Rs: 1,500,000+</option>
+    <label>Wedding Budget</label>
+    <select class="form-control" name="weddingBudget">
+		<option value=<?php echo $weddingBudget; ?> selected ><?php echo $weddingBudget; ?></option>
+        <option>Rs:0-Rs:500,000</option>
+        <option>Rs:500,000-Rs:750,000</option>
+        <option>Rs:750,000-Rs:1,000,000</option>
+        <option>Rs:1,000,000-Rs:1,500,000</option>
+        <option>Rs:1,500,000+</option>
       </select>
 	</div>
 </div>
 
-<div class="form-row">
-	<div class="form-group col-md-6">
-		<label for="inputfirstname">Desired Ceremony Setting - Choose 1</label>
-		<div class="radio">
-        <label><input type="radio" name="optradio" checked>     Option 1</label>
-        </div>
-        <div class="radio">
-        <label><input type="radio" name="optradio">     Option 2</label>
-        </div>
-        <div class="radio disabled">
-        <label><input type="radio" name="optradio">     Option 3</label>
-</div>
-	</div>
-</div>
 
+<button type="submit" class="btn btn-outline-info btn-rounded waves-effect"> UPDATE </button>
 </form>
-
-<button type="button" class="btn btn-outline-info btn-rounded waves-effect">Back To Dashboard</button>
-
+<button type="button" class="btn btn-outline-info btn-rounded waves-effect"><a href="<?php echo base_url(); ?>index.php/Home/CustomerDashboard" >Back To Dashboard</a> </button>
 
